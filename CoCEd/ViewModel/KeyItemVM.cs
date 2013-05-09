7e28838114b1ddbc1424acec7790d8a15b66bb25
perdevcoc <perdevcoc@b9d9d289-476f-466f-926b-5323db441fa4>
2013-05-09 18:21:29 +0000

    Import missing perks.

--- CoCEd/ViewModel/KeyItemVM.cs
+++ CoCEd/ViewModel/KeyItemVM.cs
@@ -11,26 +11,14 @@ namespace CoCEd.ViewModel
 {
     public sealed class KeyItemVM : NamedVector4VM
     {
-        readonly string _name;
-
         public KeyItemVM(AmfObject keyItems, string name)
-            : base(keyItems, XmlData.Instance.KeyItems.FirstOrDefault(x => x.Name == name))
+            : base(keyItems, XmlData.Instance.KeyItems.FirstOrDefault(x => x.Name == name), name)
         {
-            _name = name;
-        }
-
-        public override string Name
-        {
-            get { return _name; }
         }
 
         protected override void InitializeObject(AmfObject obj)
         {
             obj["keyName"] = _name;
-            obj["value1"] = 0;
-            obj["value2"] = 0;
-            obj["value3"] = 0;
-            obj["value4"] = 0;
         }
 
         protected override bool IsObject(AmfObject obj)
--- CoCEd/ViewModel/StatusVM.cs
+++ CoCEd/ViewModel/StatusVM.cs
@@ -11,26 +11,14 @@ namespace CoCEd.ViewModel
 {
     public sealed class StatusVM : NamedVector4VM
     {
-        readonly string _name;
-
         public StatusVM(AmfObject allStatuses, string name)
-            : base(allStatuses, XmlData.Instance.Statuses.FirstOrDefault(x => x.Name == name))
+            : base(allStatuses, XmlData.Instance.Statuses.FirstOrDefault(x => x.Name == name), name)
         {
-            _name = name;
-        }
-
-        public override string Name
-        {
-            get { return _name; }
         }
 
         protected override void InitializeObject(AmfObject obj)
         {
             obj["statusAffectName"] = _name;
-            obj["value1"] = 0;
-            obj["value2"] = 0;
-            obj["value3"] = 0;
-            obj["value4"] = 0;
         }
 
         protected override bool IsObject(AmfObject obj)
