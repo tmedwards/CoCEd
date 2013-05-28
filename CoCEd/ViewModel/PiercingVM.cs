6d62d82ff89c2c7ca9163842c1a4bace8a301cfd
bobbaganoosh <bobbaganoosh@b9d9d289-476f-466f-926b-5323db441fa4>
2013-05-28 05:37:58 +0000

    Improved the Piercing control to allow users to choose Material, too. Added logic to generate the piercing name based on piercing material and type. Added Magical Fertility and Magical Virility perks to CoCEd.xml.

--- CoCEd/ViewModel/PiercingVM.cs
+++ CoCEd/ViewModel/PiercingVM.cs
@@ -9,16 +9,53 @@ namespace CoCEd.ViewModel
     public sealed class PiercingVM : ObjectVM
     {
         readonly string _prefix;
+        private bool hasChangedMaterial;
 
         public PiercingVM(AmfObject obj, string prefix)
             : base(obj)
         {
             _prefix = prefix;
+            hasChangedMaterial = GetString(_prefix == "" ? "pLong" : _prefix + "PLong").Length > 0;
         }
 
         public IEnumerable<XmlEnum> AllTypes
         {
             get { return XmlData.Instance.Body.PiercingTypes; }
+        }
+
+        public IEnumerable<XmlEnum> AllMaterials
+        {
+            get { return XmlData.Instance.Body.PiercingMaterials; }
+        }
+
+        public string AllowedTypes
+        {
+            get
+            {
+                switch(_prefix)
+                {
+                    case "":
+                        return "Game-allowed cock piercing types are stud, ring, and ladder.";
+                    case "clit":
+                        return "Game-allowed clit piercing types are stud and ring.";
+                    case "ears":
+                        return "Game-allowed ear piercing types are stud, ring, and hoop.";
+                    case "eyebrow":
+                        return "Game-allowed eyebrow piercing types are stud and ring.";
+                    case "lip":
+                        return "Game-allowed lip piercing types are stud and ring.";
+                    case "nose":
+                        return "Game-allowed nose piercing types are stud and ring.";
+                    case "tongue":
+                        return "Game-allowed tongue piercing type is stud.";
+                    case "nipples":
+                        return "Game-allowed nipple piercing types are stud, ring, and chain.";
+                    case "labia":
+                        return "Game-allowed labia piercing types are stud and ring.";
+                    default:
+                        return "This PiercingVM did not have a prefix passed to its constructor. Please post about this on the forum.";
+                }
+            }
         }
 
         public int Type
@@ -27,9 +64,91 @@ namespace CoCEd.ViewModel
             set
             {
                 SetValue(_prefix == "" ? "pierced" : _prefix + "Pierced", value);
+                if (value != 0 && Material != 0)
+                {
+                    GeneratePiercingName(value, Material);
+                }
                 OnPropertyChanged("CanEditName");
                 OnPropertyChanged("Label");
             }
+        }
+
+        public int Material
+        {
+            get 
+            {
+                if(hasChangedMaterial)
+                {
+                    string name = GetString(_prefix == "" ? "pLong" : _prefix + "PLong");
+                    string material = name.Substring(0, name.IndexOf(' ')); //does not work well for Ceraph's piercings
+                    var xmlMaterial = XmlData.Instance.Body.PiercingMaterials.FirstOrDefault(x => x.Name == material);
+                    if (xmlMaterial != null) return xmlMaterial.ID;
+                }
+                return 0;
+            }
+            set
+            {
+                if (Type != 0 && value != 0)
+                {
+                    hasChangedMaterial = true;
+                    GeneratePiercingName(Type, value);
+                }
+                OnPropertyChanged("Label");
+            }
+        }
+
+        private void GeneratePiercingName(int type, int material)
+        {
+            if (type == 0 || material == 0)
+            {
+                //Set UpperName
+                SetValue(_prefix == "" ? "pLong" : _prefix + "PLong", "");
+                //Set LowerName
+                SetValue(_prefix == "" ? "pShort" : _prefix + "PShort", "");
+            }
+            var xmlMaterial = XmlData.Instance.Body.PiercingMaterials.FirstOrDefault(x => x.ID == material);
+            string upper = xmlMaterial.Name;
+            string lower = xmlMaterial.Name.ToLower();
+            string rest = " ";
+            //location
+            if (_prefix == "")
+            {
+                rest += type == 3 ? "" : "cock-";
+            }
+            else if (_prefix == "nipples")
+            {
+                rest += "nipple-";
+            }
+            else if (_prefix == "ears")
+            {
+                rest += "ear";
+            }
+            else
+            {
+                rest += _prefix + "-";
+            }
+            //type
+            if (type == 3)
+            {
+                rest += "jacob's ladder";
+            }
+            else if (type == 5)
+            {
+                rest += "chain";
+            }
+            else
+            {
+                var xmlType = XmlData.Instance.Body.PiercingTypes.FirstOrDefault(x => x.ID == Type);
+                if (xmlType != null) rest += xmlType.Name.ToLower();
+                if (_prefix == "ears" || _prefix == "nipples" || _prefix == "labia")
+                {
+                    rest += "s";
+                }
+            }
+            //Set UpperName
+            SetValue(_prefix == "" ? "pLong" : _prefix + "PLong", upper + rest);
+            //Set LowerName
+            SetValue(_prefix == "" ? "pShort" : _prefix + "PShort", lower + rest);
         }
 
         public string UpperName
