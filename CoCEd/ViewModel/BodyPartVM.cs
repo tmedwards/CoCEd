1d8f703fcbd324a21aa7d10ba0e16c2008846a4f
hormelcookies <70467971+hormelcookies@users.noreply.github.com>
2021-03-13 18:22:27 -0800

    Fix body parts

---
+++ CoCEd/ViewModel/BodyPartVM.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoCEd.Model;
+
+namespace CoCEd.ViewModel
+{
+
+    public abstract class BodyPartVM : ObjectVM
+    {
+        protected readonly GameVM _game;
+        public abstract string Name { get; }
+        public BodyPartVM(GameVM game, AmfObject obj) : base(obj) => _game = game;
+
+
+        public AmfObject GetSelf()
+        {
+            return _obj;
+        }
+
+        public abstract IEnumerable<XmlEnum> AllTypes
+        {
+            get;
+        }
+        protected int GenericGetIntProp(string prop)
+        {
+            if (prop == "")
+            {
+                return -1;
+            }
+            if (_game.IsXianxia)
+            {
+
+                return GetInt(prop, 0);
+            }
+            string camelCasedProp = prop.Substring(0, 1).ToUpper() + prop.Substring(1);
+            return _game.GetInt(Name + camelCasedProp, 0);
+        }
+        protected string GenericGetStringProp(string prop)
+        {
+            if (prop == "")
+            {
+                return "";
+            }
+            if (_game.IsXianxia)
+            {
+
+                return GetString(prop);
+            }
+            string camelCasedProp = prop.Substring(0, 1).ToUpper() + prop.Substring(1);
+            return _game.GetString(Name + camelCasedProp);
+        }
+
+        protected void GenericSetProp(string prop, object value)
+        {
+            if (_game.IsXianxia)
+            {
+                SetValue(prop, value);
+            }
+            string camelCasedProp = prop.Substring(0, 1).ToUpper() + prop.Substring(1);
+            _game.SetValue(Name + camelCasedProp, value);
+        }
+
+        virtual public int Type
+        {
+            get {
+                return GenericGetIntProp("type");
+            }
+            set {
+                GenericSetProp("type", value);
+            }
+        }
+    }
+    public class FaceVM : BodyPartVM
+    {
+        public override string Name { get { return "face"; } }
+        public FaceVM(GameVM game, AmfObject obj) : base(game, obj) { }
+
+        public override IEnumerable<XmlEnum> AllTypes
+        {
+            get
+            {
+                foreach (var type in XmlData.Current.Body.FaceTypes)
+                {
+                    yield return type;
+                }
+            }
+        }
+    }
+    public class LowerBodyVM : BodyPartVM
+    {
+        public override string Name { get { return "lowerBody"; } }
+        public LowerBodyVM(GameVM game, AmfObject obj) : base(game, obj) { }
+
+        public override IEnumerable<XmlEnum> AllTypes
+        {
+            get
+            {
+                foreach (var type in XmlData.Current.Body.LowerBodyTypes)
+                {
+                    yield return type;
+                }
+            }
+        }
+        public int LegConfigs
+        {
+            get
+            {
+                if (!HasLegConfigs) return -1;
+                return (LegCount / 2) - 1;
+            }
+            set
+            {
+                if (_game.IsRevampOrXianxia)
+                {
+                    if (value == LegConfigs) return;
+                    LegCount = (value + 1) * 2;
+                }
+            }
+        }
+
+        //named legCount in both xianxia and base
+        public int LegCount
+        {
+            get {
+                if (_game.IsXianxia)
+                {
+                    return GetInt("legCount", 0);
+                }
+                // lowerBody
+                return _game.GetInt("legCount", 0);
+            }
+            set {
+                if (_game.IsXianxia)
+                {
+                    SetValue("legCount", value);
+                }
+                else
+                {
+                    _game.SetValue("legCount", value);
+                }
+            }
+        }
+        public override int Type
+        {
+            get {
+                if (_game.IsXianxia)
+                {
+                    return GetInt("type", 0);
+                }
+                // lowerBody
+                return _game.GetInt("lowerBody", 0);
+            }
+            set
+            {
+                int type = value;
+                int count = LegCount; 
+                // Set the default `LegCount` value when the lower body type is changed.
+                switch (value)
+                {
+                    case 3: // Naga
+                    case 8: // Goo
+                    case 51: // Hydra
+                        count = 1;
+                        break;
+
+                    case 1: //Hooves
+                    case 21: //Cloven Hooves
+                        if (count != 4) count = 2;
+                        break;
+
+                    case 11: // Pony
+                        count = 4;
+                        break;
+
+                    case 4: // Centaur, deprecated
+                        count = 4;
+                        type = 1; // change to Hoofed
+                        break;
+
+                    case 24: // Deertaur, deprecated
+                        count = 4;
+                        type = 21; // change to Cloven Hoofed
+                        break;
+
+                    case 16: // Drider
+                        count = 8;
+                        break;
+
+                    default:
+                        count = 2;
+                        break;
+                }
+                LegCount = count;
+                if (_game.IsXianxia)
+                {
+                    SetValue("type", type);
+                }
+                else
+                {
+                    _game.SetValue("lowerBody", value);
+                }
+
+                OnPropertyChanged("LegCount");
+                OnPropertyChanged("LegConfigs");
+                OnPropertyChanged("HasLegConfigs");
+            }
+        }
+        public bool HasLegConfigs
+        {
+            get
+            {
+                if (_game.IsVanilla)
+                    return false;
+                if (_game.IsXianxia)
+                {
+                    switch (Type)
+                    {
+                        // Types which definitely have only a single allowed leg configuration.
+                        case 1: // Hoofed
+                        case 21: // Cloven Hoofed
+                            return true;
+                        default:
+                            return false;
+                    }
+                }
+                switch (Type)
+                {
+                    // Types which definitely have only a single allowed leg configuration.
+                    case 0: // Human (biped)
+                    case 3: // Naga (uniped)
+                    case 8: // Goo (uniped)
+                    case 11: // Pony (quadruped)
+                    case 16: // Drider (octoped) → Biped form is lower body type #15 (Chitinous spider legs).
+                        return false;
+
+                    // Types which probably should have only a single allowed leg configuration.
+                    case 5: // Demonic high-heels (biped)    → Funny human legs, should have the same limitations as lower body type #0 (Human).
+                    case 6: // Demonic claws (biped)         → (same as the above)
+                    case 7: // Bee (biped)                   → (same as the above)
+                    case 13: // Harpy (biped)                 → (same as the above)
+                    case 15: // Chitinous spider legs (biped) → Octoped form is lower body type #16 (Drider).
+                        return false;
+
+                    // Types which I'm unsure about, but I'm allowing because the game currently does.
+                    // #14 Kangaroo (triped w/ pentapedal & bipedal-hopping locomotion) → Unsure.  Kangaroos are weird.
+
+                    // All other types may have either biped or quadruped leg configurations.
+                    default:
+                        return true;
+                }
+            }
+        }
+    }
+
+    public class TailVM : BodyPartVM
+    {
+        public TailVM(GameVM game, AmfObject obj) : base(game, obj) { }
+        public override string Name { get { return "tail"; } }
+
+        public override IEnumerable<XmlEnum> AllTypes
+        {
+            get
+            {
+                foreach (var type in XmlData.Current.Body.TailTypes)
+                {
+                    yield return type;
+                }
+            }
+        }
+        public bool IsTailVenomEnabled
+        {
+            get { return (Type == 5 || Type == 6); }
+        }
+        public bool IsTailCountEnabled
+        {
+            get { return (Type == 13); }
+        }
+
+
+        public bool IsTailRechargeEnabled
+        {
+            get { return (Type == 5 || Type == 6); }
+        }
+
+        public int Count
+        {
+            get { 
+                if (_game.IsXianxia)
+                    return GetInt("count", 0); 
+                if (IsTailCountEnabled)
+                    return _game.GetInt("tailVenum", 0);
+                return 0;
+            }
+            set {
+                if (_game.IsXianxia)
+                    SetValue("count", value);
+                if (IsTailCountEnabled)
+                    _game.SetValue("tailVenum", value);
+            }
+        }
+        public int Venom
+        {
+            get
+            {
+                if (_game.IsXianxia)
+                    return GetInt("venom", 0);
+                if (IsTailVenomEnabled)
+                    return _game.GetInt("tailVenum", 0);
+                return 0;
+            }
+            set
+            {
+                if (_game.IsXianxia)
+                    SetValue("venom", value);
+                if (IsTailVenomEnabled)
+                    _game.SetValue("tailVenum", value);
+            }
+        }
+        public int Recharge
+        {
+            get { return GenericGetIntProp("recharge"); }
+            set { GenericSetProp("recharge", value); }
+        }
+        public override int Type
+        {
+            get { return GetInt("type", 0); }
+            set
+            {
+                int type = value;
+                int count = GetInt("count", 0);
+                if (count < 1 && type != 13) // If not Fox (which can have multiple tails), set count to 1;
+                {
+                    count = 1;
+                }
+                SetValue("count", count);
+                SetValue("type", type);
+            }
+        }
+
+    }
+
+    public class ClawsVM: BodyPartVM
+    {
+        public override string Name { get { return "claw"; } }
+
+        public ClawsVM(GameVM game, AmfObject obj) : base(game, obj) { }
+
+        public override IEnumerable<XmlEnum> AllTypes
+        {
+            get
+            {
+                foreach (var type in XmlData.Current.Body.ClawTypes)
+                {
+                    yield return type;
+                }
+            }
+        }
+
+        public string Tone
+        {
+            get { return GenericGetStringProp("tone"); }
+            set { GenericSetProp("tone", value); }
+        }
+
+        public IEnumerable<string> AllTones
+        {
+            get
+            {
+                foreach (var tone in XmlData.Current.Body.ClawTones)
+                {
+                    yield return tone;
+                }
+            }
+        }
+
+    }
+}
---
+++ CoCEd/ViewModel/SkinVM.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoCEd.Model;
+
+namespace CoCEd.ViewModel
+{
+    public class SkinLayerVM : BodyPartVM
+    {
+        private readonly string _name;
+        public override string Name { get { return _name; } }
+
+        public SkinLayerVM(GameVM game, AmfObject obj, string name) : base(game, obj) => _name = name;
+
+        public string DefaultAdjective()
+        {
+            if (Type == 3) // Goo
+            {
+                return "goopey";
+            }
+            return "";
+        }
+        public string DefaultDescription()
+        {
+            var matches = AllTypes.Where(s => s.ID == Type);
+            if (matches.Count() > 0)
+            {
+                return matches.First().Name.ToLower();
+            }
+            return "skin";
+        }
+
+        public override IEnumerable<XmlEnum> AllTypes
+        {
+            get
+            {
+                if (_game.IsXianxia && _name == "coat")
+                {
+                    //return blank coat type as well
+                    yield return new XmlEnum() { Name = "", ID = 0 };
+                }
+                foreach (var type in XmlData.Current.Body.SkinTypes)
+                {
+                    if (_game.IsNotXianxia)
+                        yield return type;
+                    if (_name == "skin")
+                    {
+                        switch (type.ID)
+                        {
+                            case 0: //Plain
+                            case 3: //Goo
+                            case 7: //Stone
+                            case 19: //Aqua-rubber like
+                            case 21: //Feather
+                            case 22: //Transparent
+                                yield return type;
+                                break;
+                        }
+                    }
+                    else if (_name == "coat")
+                    {
+                        switch (type.ID)
+                        {
+                            case 1: //Fur
+                            case 5: //Chitin
+                            case 6: //Bark
+                            case 9: //Aqua scales
+                            case 14: //dragon scales
+                            case 15: //moss
+                                yield return type;
+                                break;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        public IEnumerable<XmlEnum> AllPatterns
+        {
+            get
+            {
+                /*if (_game.IsNotXianxia)
+                {
+                    yield return null;
+                }*/
+                if (_game.IsXianxia)
+                {
+                    foreach (var type in XmlData.Current.Body.SkinPatterns)
+                    {
+                        if (_game.IsNotXianxia)
+                            yield return type;
+                        if (_name == "skin")
+                        {
+                            switch (type.ID)
+                            {
+                                case 0: //None
+                                case 1: //Magical Tattoo
+                                case 2: //Orca Underbody
+                                case 5: //Battle Tattoo
+                                case 7: //Lightning chaped tattoo
+                                case 9: //Scar shaped tattoo
+                                case 10: //White/black veins
+                                case 11: //Venomous markings
+                                case 12: //USHI_ONI_ONNA_TATTOO
+                                case 13: //SCAR_WINDSWEPT
+                                case 14: //Oil
+                                    yield return type;
+                                    break;
+                            }
+                        }
+                        else if (_name == "coat")
+                        {
+                            switch (type.ID)
+                            {
+                                case 0: //None
+                                case 3: //Bee Stripes
+                                case 4: //tiger Stripes
+                                case 6: //Spotted
+                                case 8: //Red Panda Underbody
+                                    yield return type;
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public override int Type
+        {
+            get {
+                return GenericGetIntProp("type");
+            }
+            set
+            {
+                int currentType = Type;
+                if (currentType != value)
+                {
+                    GenericSetProp("type", value);
+                    Description = DefaultDescription();
+                    Pattern = 0;
+                    Adjective = DefaultAdjective();
+                    OnPropertyChanged("Type");
+                    OnPropertyChanged("Description");
+                    OnPropertyChanged("Pattern");
+                    OnPropertyChanged("Adjective");
+                    _game.Skin.PokeCoatEnabled();
+                }
+            }
+        }
+
+        public string Color
+        {
+            get
+            {
+                if (_game.IsXianxia)
+                {
+                    var curColor = GetString("color");
+                    if (curColor == null || curColor == "")
+                    {
+                        return _game.HairColor;
+                    }
+                    return GetString("color");
+                }
+
+                //if not xianxia
+                if (Name == "skin")
+                {
+                    return _game.GetString("skinTone");
+                }
+                return _game.GetString("furColor");
+            }
+            set
+            {
+                if (_game.IsXianxia)
+                {
+                    var curColor = GetString("color");
+                    SetValue("color", value);
+
+                    // if color 2 was set to the same color, change that too
+                    if (curColor == Color2)
+                    {
+                        Color2 = Color;
+                    }
+                }
+                // not xianxia
+                if (Name == "skin")
+                {
+                    _game.SetValue("skinTone", value);
+                }
+                else
+                {
+                    _game.SetValue("furColor", value);
+                }
+            }
+        }
+        public int Pattern
+        {
+            get { 
+                if (_game.IsNotXianxia)
+                {
+                    return 0;
+                }
+                return GetInt("pattern", 0);
+            }
+            set
+            {
+                if (_game.IsXianxia)
+                {
+                    SetValue("pattern", value);
+                }
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                string desc = GenericGetStringProp("desc");
+                if (desc == null)
+                {
+                    return DefaultDescription();
+                }
+                return desc;
+            }
+            set { GenericSetProp("desc", value); }
+
+        }
+        public string Adjective
+        {
+            get
+            {
+                string adj = GenericGetStringProp("adj");
+                if (adj == null)
+                {
+                    return DefaultAdjective();
+                }
+                return adj;
+            }
+            set { GenericSetProp("adj", value); }
+        }
+
+        public string Color2
+        {
+            get
+            {
+                if (GetString("color2") == null || GetString("color2") == "")
+                {
+                    return Color;
+                }
+                return GetString("color2");
+            }
+            set
+            {
+                if (value == Color)
+                {
+                    SetValue("color2", "");
+                }
+                SetValue("color2", value);
+            }
+        }
+    }
+
+    public class SkinVM : BodyPartVM
+    {
+        //Do not use this
+        public override int Type { get { throw new Exception("don't get skinVM type"); } set { throw new Exception("don't set skinVM type"); } }
+        public override string Name { get { return "skin"; } }
+        public SkinVM(GameVM game, AmfObject obj) : base(game, obj)
+        {
+            if (_obj == null || game.IsNotXianxia)
+            {
+                Base = new SkinLayerVM(game, null, "skin");
+                Coat = new SkinLayerVM(game, null, "coat");
+                return;
+            }
+            var _baseobj = GetObj("base");
+            if (_baseobj != null)
+            {
+                Base = new SkinLayerVM(game, _baseobj, "skin");
+            }
+            else
+            {
+                Base = new SkinLayerVM(game, new AmfObject(AmfTypes.Dictionary), "skin");
+                SetValue("base", Base.GetSelf());
+            }
+            var _coatobj = GetObj("coat");
+            if (_coatobj != null)
+            {
+                Coat = new SkinLayerVM(game, _coatobj, "coat");
+            }
+            else
+            {
+                Coat = new SkinLayerVM(game, new AmfObject(AmfTypes.Dictionary), "coat");
+                SetValue("coat", Coat.GetSelf());
+            }
+        }
+
+        public override IEnumerable<XmlEnum> AllTypes
+        {
+            get
+            {
+                foreach (var type in XmlData.Current.Body.SkinTypes)
+                {
+                    yield return type;
+                }
+            }
+        }
+        public IEnumerable<XmlEnum> AllPatterns
+        {
+            get
+            {
+                foreach (var type in XmlData.Current.Body.SkinPatterns)
+                {
+                    yield return type;
+                }
+            }
+        }
+
+        public int Coverage
+        {
+            get { 
+                if (_game.IsXianxia)
+                    return GetInt("coverage", 0);
+                return 0;
+            }
+            set {
+                if (_game.IsXianxia)
+                {
+                    SetValue("coverage", value);
+                    if (value > 0 && Coat.Type == 0)
+                    {
+                        Coat.Type = 1;
+                    }
+                    else if (value == 0)
+                    {
+                        Coat.Type = 0;
+                    }
+                    OnPropertyChanged("IsCoatEnabled");
+                }
+            }
+        }
+        public void PokeCoatEnabled()
+        {
+            OnPropertyChanged("IsCoatEnabled");
+
+        }
+        public bool IsCoatEnabled
+        {
+            get
+            {
+                if (_game.IsXianxia)
+                {
+                    return Coverage > 0;
+                }
+                else
+                {
+                    return Base.Type == 1;
+                }
+            }
+        }
+        public SkinLayerVM Base { get; private set; }
+        public SkinLayerVM Coat { get; private set; }
+    }
+
+}
